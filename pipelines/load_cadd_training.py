d2a0f7fed3bbd12d61e5c2cb86e4c1b479dd7a84
simancas <simancas@in.tum.de>
2018-10-25 13:01:49 +0200

    Initializing repo

---
+++ pipelines/load_cadd_training.py
@@ -0,0 +1,4 @@
+import dask.dataframe as dd
+
+cadd_df = dd.read_csv('/s/databases/cadd/v1.3/krishna.gs.washington.edu/' + '
+                      members/mkircher/download/CADD/v1.3/training_data')
